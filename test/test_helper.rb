ec80b2408398198a912e5a85111c238fbc28ed12
Chris Schneider <chris@scoutapp.com>
2015-08-13 14:20:58 -0600

    Add helper const_unset method to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,12 @@ require 'minitest/pride'
 
 require 'pry'
 
+Kernel.module_eval do
+  # Unset a constant without private access.
+  def self.const_unset(const)
+    self.instance_eval { remove_const(const) }
+  end
+end
+
 # require 'scout_apm'
 
