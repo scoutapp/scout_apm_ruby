9b956bc6d5d0b34a92238edc4502fb61c1c1eecd
Chris Schneider <chris@scoutapp.com>
2015-08-21 11:39:51 -0600

    Split module in StackItem

--- lib/scout_apm/stack_item.rb
+++ lib/scout_apm/stack_item.rb
@@ -1,18 +1,20 @@
-class ScoutApm::StackItem
-  attr_accessor :children_time
-  attr_reader :metric_name, :start_time
+module ScoutApm
+  class StackItem
+    attr_accessor :children_time
+    attr_reader :metric_name, :start_time
 
-  def initialize(metric_name)
-    @metric_name = metric_name
-    @start_time = Time.now
-    @children_time = 0
-  end
+    def initialize(metric_name)
+      @metric_name = metric_name
+      @start_time = Time.now
+      @children_time = 0
+    end
 
-  def ==(o)
-    self.eql?(o)
-  end
+    def ==(o)
+      self.eql?(o)
+    end
 
-  def eql?(o)
-    self.class == o.class && metric_name.eql?(o.metric_name)
-  end
-end # class StackItem
+    def eql?(o)
+      self.class == o.class && metric_name.eql?(o.metric_name)
+    end
+  end # class StackItem
+end
