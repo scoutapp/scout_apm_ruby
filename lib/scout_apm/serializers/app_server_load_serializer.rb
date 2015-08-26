c19b7aab74d27691fe94fdd8fc3eaa2b1ac618c5
Chris Schneider <chris@scoutapp.com>
2015-08-26 10:31:44 -0600

    Add AppServerLoad to collect and send the on-boot info

---
+++ lib/scout_apm/serializers/app_server_load_serializer.rb
@@ -0,0 +1,15 @@
+# Serialize & deserialize commands from the APM server to the instrumented app
+
+module ScoutApm
+  module Serializers
+    class AppServerLoadSerializer
+      def self.serialize(data)
+        Marshal.dump(data)
+      end
+
+      def self.deserialize(data)
+        Marshal.load(data)
+      end
+    end
+  end
+end
