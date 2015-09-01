6972f11d9ecab1d373de7936d4394e3eaa915c92
Chris Schneider <chris@scoutapp.com>
2015-09-01 15:37:49 -0600

    Fix Puma integration

--- lib/scout_apm/server_integrations/puma.rb
+++ lib/scout_apm/server_integrations/puma.rb
@@ -11,14 +11,21 @@ module ScoutApm
         :puma
       end
 
-      def forking?; true; end
+      def forking?
+        return false unless defined?(::Puma)
+        options = ::Puma.cli_config.instance_variable_get(:@options)
+        logger.debug options.inspect
+        options[:preload_app]
+      rescue
+        false
+      end
 
       def present?
-        defined?(::Puma) && File.basename($0) == 'puma'
+        defined?(::Puma) && (File.basename($0) =~ /\Apuma/)
       end
 
       def install
-        Puma.cli_config.options[:before_worker_boot] << Proc.new do
+        ::Puma.cli_config.options[:before_worker_boot] << Proc.new do
           logger.debug "Installing Puma worker loop."
           ScoutApm::Agent.instance.start_background_worker
         end
