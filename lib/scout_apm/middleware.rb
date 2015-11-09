b4a5027791ca83f05ab44bcd758a55732f95c0ea
Chris Schneider <chris@scoutapp.com>
2015-11-09 16:11:57 -0700

    Clarify and fix an issue with the middleware instrumentation
    The `ensure` block was running, but not becoming the return value of the
    method, causing the middleware directly above to receive nil, instead of
    a rack environment, making it fail.

--- lib/scout_apm/middleware.rb
+++ lib/scout_apm/middleware.rb
@@ -5,7 +5,7 @@ module ScoutApm
     def initialize(app)
       @app = app
       @attempts = 0
-      @started = false
+      @started = ScoutApm::Agent.instance.started?
     end
 
     # If we get a web request in, then we know we're running in some sort of app server
@@ -13,18 +13,19 @@ module ScoutApm
       if @started || @attempts > MAX_ATTEMPTS
         @app.call(env)
       else
-        begin
-          @attempts += 1
-          ScoutApm::Agent.instance.start(:skip_app_server_check => true)
-          @started = ScoutApm::Agent.instance.started?
-        rescue => e
-          # Can't be sure of any logging here, so fall back to ENV var and STDOUT
-          if ENV["SCOUT_LOG_LEVEL"] == "debug"
-            STDOUT.puts "Failed to start via Middleware: #{e.message}\n\t#{e.backtrace.join("\n\t")}"
-          end
-        ensure
-          @app.call(env)
-        end
+        attempt_to_start_agent
+        @app.call(env)
+      end
+    end
+
+    def attempt_to_start_agent
+      @attempts += 1
+      ScoutApm::Agent.instance.start(:skip_app_server_check => true)
+      @started = ScoutApm::Agent.instance.started?
+    rescue => e
+      # Can't be sure of any logging here, so fall back to ENV var and STDOUT
+      if ENV["SCOUT_LOG_LEVEL"] == "debug"
+        STDOUT.puts "Failed to start via Middleware: #{e.message}\n\t#{e.backtrace.join("\n\t")}"
       end
     end
   end
