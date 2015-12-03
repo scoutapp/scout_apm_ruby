f46adac89bbb88eba43fa3e6082de080c5995c7b
Chris Schneider <chris@scoutapp.com>
2015-12-03 11:17:32 -0700

    Initial attempt at using TrackedRequest & Layer in ActionControllerRails3Instruments

--- lib/scout_apm/instruments/action_controller_rails_3.rb
+++ lib/scout_apm/instruments/action_controller_rails_3.rb
@@ -20,55 +20,62 @@ module ScoutApm
         if defined?(::ActionController) && defined?(::ActionController::Metal)
           ScoutApm::Agent.instance.logger.info "Instrumenting ActionController::Metal"
           ::ActionController::Metal.class_eval do
-            include ScoutApm::Tracer
+            # include ScoutApm::Tracer
             include ScoutApm::Instruments::ActionControllerRails3Instruments
           end
         end
 
-        if defined?(::ActionView) && defined?(::ActionView::PartialRenderer)
-          ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::PartialRenderer"
-          ::ActionView::PartialRenderer.class_eval do
-            include ScoutApm::Tracer
-            instrument_method :render_partial,
-              :metric_name => 'View/#{@template.virtual_path rescue "Unknown Partial"}/Rendering',
-              :scope => true
+        # if defined?(::ActionView) && defined?(::ActionView::PartialRenderer)
+          # ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::PartialRenderer"
+          # ::ActionView::PartialRenderer.class_eval do
+            # include ScoutApm::Tracer
+            # instrument_method :render_partial,
+              # :metric_name => 'View/#{@template.virtual_path rescue "Unknown Partial"}/Rendering',
+              # :scope => true
 
-            instrument_method :collection_with_template,
-              :metric_name => 'View/#{@template.virtual_path rescue "Unknown Collection"}/Rendering',
-              :scope => true
-          end
+            # instrument_method :collection_with_template,
+              # :metric_name => 'View/#{@template.virtual_path rescue "Unknown Collection"}/Rendering',
+              # :scope => true
+          # end
 
-          ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::TemplateRenderer"
-          ::ActionView::TemplateRenderer.class_eval do
-            include ScoutApm::Tracer
-            instrument_method :render_template,
-              :metric_name => 'View/#{args[0].virtual_path rescue "Unknown"}/Rendering',
-              :scope => true
-          end
-        end
+          # ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::TemplateRenderer"
+          # ::ActionView::TemplateRenderer.class_eval do
+            # include ScoutApm::Tracer
+            # instrument_method :render_template,
+              # :metric_name => 'View/#{args[0].virtual_path rescue "Unknown"}/Rendering',
+              # :scope => true
+          # end
+        # end
       end
     end
 
     module ActionControllerRails3Instruments
-      # Instruments the action and tracks errors.
+      # TODO: Rewire stackprof
       def process_action(*args)
-        scout_controller_action = "Controller/#{controller_path}/#{action_name}"
+        req = ScoutApm::RequestManager.lookup
+        req.annotate_request(:uri => request.fullpath)
+        req.context.add_user(:ip => request.remote_ip)
 
-        self.class.scout_apm_trace(scout_controller_action, :uri => request.fullpath, :ip => request.remote_ip) do
-          Thread::current[:scout_apm_prof] = nil
-          StackProf.start(:mode => :wall, :interval => ScoutApm::Agent.instance.config.value("stackprof_interval"))
-
-          begin
-            super
-          rescue Exception
-            ScoutApm::Agent.instance.store.track!("Errors/Request", 1, :scope => nil)
-            raise
-          ensure
-            Thread::current[:scout_apm_scope_name] = nil
-            StackProf.stop
-            Thread::current[:scout_apm_prof] = StackProf.results
-          end
+        req.start_layer( ScoutApm::Layer.new("Controller", "#{controller_path}/#{action_name}") )
+        begin
+          super
+        ensure
+          req.stop_layer
         end
+
+#        self.class.scout_apm_trace(scout_controller_action, :uri => request.fullpath, :ip => request.remote_ip) do
+#          # Thread::current[:scout_apm_prof] = nil
+#          # StackProf.start(:mode => :wall, :interval => ScoutApm::Agent.instance.config.value("stackprof_interval"))
+#
+#          begin
+#            super
+#          rescue Exception
+#            raise
+#          ensure
+#            Thread::current[:scout_apm_scope_name] = nil
+#            # StackProf.stop
+#            # Thread::current[:scout_apm_prof] = StackProf.results
+#          end
       end
     end
   end
