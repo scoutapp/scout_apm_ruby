d27c2f050250b34f424af906bbfb373174feccde
Chris Schneider <chris@scoutapp.com>
2016-02-17 16:49:53 -0700

    Use alias_method to instrument sidekiq.

--- lib/scout_apm/background_job_integrations/sidekiq.rb
+++ lib/scout_apm/background_job_integrations/sidekiq.rb
@@ -20,18 +20,23 @@ module ScoutApm
         SidekiqMiddleware.class_eval do
           include ScoutApm::Tracer
         end
+
         ::Sidekiq.configure_server do |config|
           config.server_middleware do |chain|
             chain.add SidekiqMiddleware
           end
         end
+
         require 'sidekiq/processor' # sidekiq v4 has not loaded this file by this point
+
         ::Sidekiq::Processor.class_eval do
-          old = instance_method(:initialize)
-          define_method(:initialize) do |boss|
-            ScoutApm::Agent.instance.start_background_worker
-            old.bind(self).call(boss)
+          def initialize_with_scout(boss)
+            ::ScoutApm::Agent.instance.start_background_worker unless ::ScoutApm::Agent.instance.background_worker_running?
+            initialize_without_scout(boss)
           end
+
+          alias_method :initialize_without_scout, :initialize
+          alias_method :initialize, :initialize_with_scout
         end
       end
     end
@@ -42,8 +47,12 @@ module ScoutApm
         job_class = msg_args["job_class"]
         latency = (Time.now.to_f - (msg['enqueued_at'] || msg['created_at'])) * 1000
 
-        ScoutApm::Agent.instance.store.track_one!("Queue", queue, 0, {:extra_metrics => {:latency => latency}})
         req = ScoutApm::RequestManager.lookup
+
+        req.start_layer( ScoutApm::Layer.new("Queue", queue) )
+
+        ScoutApm::Agent.instance.store.track_one!("Queue", "Latency", latency)
+
         req.start_layer( ScoutApm::Layer.new("Job", job_class) )
 
         begin
