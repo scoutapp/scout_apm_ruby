1d41e35ca77e470ad133fd58680f400dfdacc06a
David Anderson <dave@dubkat.com>
2016-02-22 21:09:11 -0700

    false unless there is gc_data

--- lib/scout_apm/stack_profile.rb
+++ lib/scout_apm/stack_profile.rb
@@ -4,10 +4,12 @@ class StackProfile
     attr_reader :gc_data
 
     def rss_increased?
+      return false unless @gc_data
       @gc_data[:end_max_rss] > @gc_data[:start_max_rss]
     end
 
     def gc_ended_between?(start_time, stop_time)
+      return false unless @gc_data
       start_time < @gc_data[:end_time] && @gc_data[:end_time] < stop_time
     end
 end
