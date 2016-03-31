947ea12f743fa2ad05cdd4a9c02587aeeb8a1a73
Chris Schneider <chris@scoutapp.com>
2016-03-31 16:36:53 -0600

    Add SlowJobRecord to store information about a given slow job

--- lib/scout_apm/slow_job_record.rb
+++ lib/scout_apm/slow_job_record.rb
@@ -2,20 +2,32 @@ module ScoutApm
   class SlowJobRecord
     attr_reader :queue_name
     attr_reader :job_name
-    attr_reader :total_call_time
-    attr_reader :total_exclusive_time
+
+    # When did this job occur
+    attr_reader :time
+
+    # What else interesting did we learn?
+    attr_reader :context
+
+    attr_reader :total_time
+    attr_reader :exclusive_time
+    alias_method :total_call_time, :total_time
+
     attr_reader :metrics
 
-    def initialize(queue_name, job_name, total_time, exclusive_time, metrics)
+    def initialize(queue_name, job_name, time, total_time, exclusive_time, context, metrics)
       @queue_name = queue_name
       @job_name = job_name
+      @time = time
       @total_time = total_time
       @exclusive_time = exclusive_time
+      @context = context
       @metrics = metrics
     end
 
     def metric_name
       "Job/#{queue_name}/#{job_name}"
     end
+
   end
 end
