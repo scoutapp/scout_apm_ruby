11d6fb24bcd323056a93d89b34406476eac691b4
Chris Schneider <chris@scoutapp.com>
2016-05-04 14:48:15 -0600

    Remove inaccurate comment

--- lib/scout_apm/layer_converters/allocation_metric_converter.rb
+++ lib/scout_apm/layer_converters/allocation_metric_converter.rb
@@ -3,9 +3,6 @@ module ScoutApm
     class AllocationMetricConverter < ConverterBase
       def call
         scope = scope_layer
-
-        # Should we mark a request as errored out if a middleware raises?
-        # How does that interact w/ a tool like Sentry or Honeybadger?
         return {} unless scope
 
         meta = MetricMeta.new("ObjectAllocations", {:scope => scope.legacy_metric_name})
