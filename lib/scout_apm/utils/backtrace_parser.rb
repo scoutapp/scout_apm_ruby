432a66fbf82d0f1c1df6c39366bf33c23fa61061
Chris Schneider <chris@scoutapp.com>
2016-08-11 11:19:30 -0600

    Adjust BacktraceParser to strip off all leading Rails root, but no more
    Given a frame's file:
      /Users/scout/secret-next-big-thing/current/app/controllers/marketing_controller.rb
    
    Before, we were attempting to be left with
    `app/controllers/marketing_controller.rb`, and it was explicitly looking
    for the `app` component of the path.
    
    Now we are looking only for the application root, allowing lib/* and
    similar files to be included as "in-app" backtrace frames.

--- lib/scout_apm/utils/backtrace_parser.rb
+++ lib/scout_apm/utils/backtrace_parser.rb
@@ -1,25 +1,27 @@
 require 'scout_apm/environment'
 
-# Removes actual values from SQL. Used to both obfuscate the SQL and group
-# similar queries in the UI.
+# Given a call stack Array, grabs the first +APP_FRAMES+ callers within the
+# application root directory.
+#
 module ScoutApm
   module Utils
     class BacktraceParser
 
       APP_FRAMES = 3 # will return up to 3 frames from the app stack.
 
-      def initialize(call_stack)
+      attr_reader :call_stack
+
+      def initialize(call_stack, root=ScoutApm::Environment.instance.root)
         @call_stack = call_stack
         # We can't use a constant as it'd be too early to fetch environment info
-        @@app_dir_regex ||= /\A(#{ScoutApm::Environment.instance.root.to_s.gsub('/','\/')}\/)(app\/(.+))/.freeze
+        @@app_dir_regex = %r|#{root}/(.*)|
       end
 
-      # Given a call stack Array, grabs the first +APP_FRAMES+ callers within the application root directory.
       def call
         stack = []
-        @call_stack.each_with_index do |c,i|
+        call_stack.each do |c|
           if m = c.match(@@app_dir_regex)
-            stack << m[2]
+            stack << m[1]
             break if stack.size == APP_FRAMES
           end
         end
---
+++ test/unit/utils/backtrace_parser_test.rb
@@ -0,0 +1,52 @@
+require_relative '../../test_helper'
+require 'scout_apm/utils/backtrace_parser'
+
+class BacktraceParserTest < Minitest::Test
+
+  ################################################################################
+  # Helpers
+
+  def root
+    "/Users/scout/secret-next-big-thing/current"
+  end
+
+  def raw_backtrace(count=10)
+    count.times.map {|i| "#{root}/app/controllers/best_#{i}_controller.rb"}
+  end
+
+  ################################################################################
+  # Tests
+
+  def test_maxes_at_APP_FRAMES
+    result = ScoutApm::Utils::BacktraceParser.new(raw_backtrace, root).call
+
+    assert_equal ScoutApm::Utils::BacktraceParser::APP_FRAMES, result.length
+  end
+
+  def test_picks_off_top_of_trace
+    result = ScoutApm::Utils::BacktraceParser.new(raw_backtrace, root).call
+
+    assert_equal false, (result[0] =~ %r|app/controllers/best_0_controller.rb|).nil?
+    assert_equal false, (result[1] =~ %r|app/controllers/best_1_controller.rb|).nil?
+    assert_equal false, (result[2] =~ %r|app/controllers/best_2_controller.rb|).nil?
+  end
+
+  def test_trims_off_root_dir
+    result = ScoutApm::Utils::BacktraceParser.new(raw_backtrace, root).call
+
+    result.each do |r|
+      assert_equal true, (r =~ %r|#{root}|).nil?
+    end
+  end
+
+  def test_works_on_shorter_backtraces
+    result = ScoutApm::Utils::BacktraceParser.new(raw_backtrace(1), root).call
+
+    assert_equal 1, result.length
+  end
+
+  def test_works_with_no_in_app_frames
+    result = ScoutApm::Utils::BacktraceParser.new(raw_backtrace, "/Users/scout/different-secrets").call
+    assert_equal 0, result.length
+  end
+end
