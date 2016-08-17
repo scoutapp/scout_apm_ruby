fdacfc12b7c80246b5b3d40d036e0ea68838f901
Chris Schneider <chris@scoutapp.com>
2016-08-17 14:00:42 -0600

    Remove incorrect layaway_test

--- test/unit/layaway_test.rb
+++ test/unit/layaway_test.rb
@@ -6,14 +6,6 @@ require 'scout_apm/context'
 require 'scout_apm/store'
 
 class LayawayTest < Minitest::Test
-  def test_merge_reporting_period
-    File.open(DATA_FILE_PATH, 'w') { |file| file.write(Marshal.dump(NEW_FORMAT)) }
-    layaway = ScoutApm::Layaway.new
-    layaway.add_reporting_period(TIMESTAMP, ScoutApm::StoreReportingPeriod.new(TIMESTAMP))
-    unmarshalled = Marshal.load(File.read(DATA_FILE_PATH))
-    assert_equal [TIMESTAMP], unmarshalled.keys
-  end
-
   TIMESTAMP = ScoutApm::StoreReportingPeriodTimestamp.new(Time.parse("2015-01-01"))
   NEW_FORMAT = {TIMESTAMP => ScoutApm::StoreReportingPeriod.new(TIMESTAMP)} # Format for 1.2+ agents
 end
