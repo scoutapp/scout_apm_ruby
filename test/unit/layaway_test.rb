f7060f2516c4ead6313226008d2d0ee2b8764ac0
Chris Schneider <chris@scoutapp.com>
2016-08-17 13:54:44 -0600

    Remove test made obsolete by previous Layaway refactoring
    We need more test around Layaway file, but this is not that test.  Now
    that we write many files, we should verify that files go to the right
    spots, with the right data.  This test still assumed everything was
    going to the same file, which made it hard to adapt.

--- test/unit/layaway_test.rb
+++ test/unit/layaway_test.rb
@@ -6,16 +6,6 @@ require 'scout_apm/context'
 require 'scout_apm/store'
 
 class LayawayTest < Minitest::Test
-  def test_add_reporting_period
-    File.open(DATA_FILE_PATH, 'w') { |file| file.write(Marshal.dump(NEW_FORMAT)) }
-    ScoutApm::Agent.instance.start
-
-    data = ScoutApm::Layaway.new
-    t = ScoutApm::StoreReportingPeriodTimestamp.new
-    data.add_reporting_period(t,ScoutApm::StoreReportingPeriod.new(t))
-    assert_equal [TIMESTAMP,t].sort_by(&:timestamp), Marshal.load(File.read(DATA_FILE_PATH)).keys.sort_by(&:timestamp)
-  end
-
   def test_merge_reporting_period
     File.open(DATA_FILE_PATH, 'w') { |file| file.write(Marshal.dump(NEW_FORMAT)) }
     layaway = ScoutApm::Layaway.new
