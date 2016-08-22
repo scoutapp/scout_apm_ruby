b2c37220f7bbf0e42b91e03e53bc8232cdf3f4b1
Chris Schneider <chris@scoutapp.com>
2016-08-22 11:15:44 -0600

    Add robustness to Middleware instrumentation - compatibility w/ ResqueWeb
    ResqueWeb (& other engines) have their own middleware stacks.  We are
    instrumenting them as well, via the same mechanism as the main Rails
    middlware stack.
    
    But ResqueWeb calls into its middleware stack, and expects exactly the
    Rails router as the top of the stack.  When we added our instrument, we
    didn't respond to the same methods, and broke when the ResqueWeb view
    attempted to call `ResqueWeb::Engine.app.url_helpers.root_path`.
    
    Two fixes here:
    
    * Method Missing any call that our MiddlewareWrapper doesn't understand.
    * Don't even install MiddlewareWrapper instrument if no middlewares were
      built during the call to `MiddlewareStack.build`.  If no middlewares
      exist, there's nothing to measure anyway.

--- lib/scout_apm/instruments/middleware_summary.rb
+++ lib/scout_apm/instruments/middleware_summary.rb
@@ -23,7 +23,15 @@ module ScoutApm
           ActionDispatch::MiddlewareStack.class_eval do
             def build_with_scout_instruments(app = nil, &block)
               mw_stack = build_without_scout_instruments(app) { block.call if block }
-              MiddlewareSummaryWrapper.new(mw_stack)
+              if app == mw_stack
+                # Return the raw middleware stack if it equaled app. No
+                # middlewares were created, so nothing to wrap & test.
+                #
+                # Avoids instrumentation of something that doesn't exist
+                mw_stack
+              else
+                MiddlewareSummaryWrapper.new(mw_stack)
+              end
             end
 
             alias_method :build_without_scout_instruments, :build
@@ -44,6 +52,36 @@ module ScoutApm
           @app.call(env)
         ensure
           req.stop_layer
+        end
+
+        # Some code (found in resque_web initially) attempts to call methods
+        # directly on `MyApplication.app`, which is the middleware stack.
+        # If it hits our middleware instead of the object at the root of the
+        # app that it expected, then a method it expects will not be there, and an
+        # error thrown.
+        #
+        # Specifically, resque_web assumes `ResqueWeb::Engine.app.url_helpers`
+        # is a method call on rails router for its own Engine, when in fact,
+        # we've added a middleware before it.
+        #
+        # So method_missing just proxies anything to the nested @app object
+        #
+        # While method_missing is not very performant, this is only here to
+        # handle edge-cases in other code, and should not be regularly called
+        def method_missing(sym, *arguments, &block)
+          if @app.respond_to?(sym)
+            @app.send(sym, *arguments, &block)
+          else
+            super
+          end
+        end
+
+        def respond_to?(sym, include_private = false)
+          if @app.respond_to?(sym, include_private)
+            true
+          else
+            super
+          end
         end
       end
     end
