a6a97e228a2e9f8e1c9bccfa53d5695007c10d08
Chris Schneider <chris@scoutapp.com>
2016-08-26 15:12:05 -0600

    Safely load scout_apm.yml for an unknown env
    Previously, we were raising an exception letting you know that the
    environment we were attempting to find in scout_apm.yml wasn't present.
    This caused problems for customers who deployed to "staging", or "testing"
    or "QA", and just didn't have a scout_apm config.
    
    Now we log the fact that we couldn't find the environment, and continue
    on.

--- test/unit/config_test.rb
+++ test/unit/config_test.rb
@@ -26,5 +26,14 @@ class ConfigTest < Minitest::Test
     assert_equal "debug", conf.value('log_level')
     assert_equal "APM Test Conf (Production)", conf.value('name')
   end
+
+  def test_loading_file_without_env_in_file
+    conf_file = File.expand_path("../../data/config_test_1.yml", __FILE__)
+    conf = ScoutApm::Config.with_file(conf_file, environment: "staging")
+
+    assert_equal "info", conf.value('log_level') # the default value
+    assert_equal nil, conf.value('name')         # the default value
+  end
 end
+
 
