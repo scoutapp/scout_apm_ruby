c448734bd92be47c05c032816905075af39cec75
Chris Schneider <chris@scoutapp.com>
2017-06-20 10:56:10 -0600

    Dynamically create controller instrument module
    We need to include these instruments at 2 places in the call stack, and
    if you `include` a module that already's been included it appears to be
    ignored.
    
    So instead, create a new module with the same content twice, to force it
    to work.
    
    The case that this is for is an API controller, to wrap the before/after
    callbacks (among other callbacks, but that's the important one).
    
    The wrapping goes top-down, a module nearer to the class being run will
    wrap everything included below it. Which is why its not possible to just
    wrap the Metal controller. We'd miss all modules between it and the
    user's code.
    
    So it starts like:
    
        YourController
        ActionController::API
        <rails-module>
        <rails-module>
        <rails-module>
        ActionController::Metal
        <rails-modules>
    
    And afterward, we need our instruments immediately after the
    ActionController::API line, and the ::Metal line.
    
        YourController
        ActionController::API
        ScoutApm::Instruments::ActionControllerRails3Rails4Instruments
        <rails-module>
        <rails-module>
        <rails-module>
        ActionController::Metal
        ScoutApm::Instruments::ActionControllerMetalInstruments
        <rails-modules>
    
    Since the Metal and API instruments are almost identical I dry'd up the
    code, and had each of the 2 Scout Instruments each include an anonymous
    module, so it looks like:
    
        YourController
        ActionController::API
        ScoutApm::Instruments::ActionControllerRails3Rails4Instruments
        <scout-instrument>
        <rails-module>
        <rails-module>
        <rails-module>
        ActionController::Metal
        ScoutApm::Instruments::ActionControllerMetalInstruments
        <scout-instrument>
        <rails-modules>

--- lib/scout_apm/instruments/action_controller_rails_3_rails4.rb
+++ lib/scout_apm/instruments/action_controller_rails_3_rails4.rb
@@ -43,6 +43,55 @@ module ScoutApm
             end
           end
         end
+
+        # Returns a new anonymous module each time it is called. So
+        # we can insert this multiple times into the ancestors
+        # stack. Otherwise it only exists the first time you include it
+        # (under Metal, instead of under API) and we miss instrumenting
+        # before_action callbacks
+      end
+
+      def self.build_instrument_module
+        Module.new do
+          def process_action(*args)
+            req = ScoutApm::RequestManager.lookup
+            current_layer = req.current_layer
+
+            # Check if this this request is to be reported instantly
+            if instant_key = request.cookies['scoutapminstant']
+              Agent.instance.logger.info "Instant trace request with key=#{instant_key} for path=#{path}"
+              req.instant_key = instant_key
+            end
+
+            if current_layer.type == "Controller"
+              # Don't start a new layer if ActionController::API or ActionController::Base handled it already.
+              STDOUT.puts "Skipping because already started controller layer"
+              super
+            else
+              req.annotate_request(:uri => ScoutApm::Instruments::ActionControllerRails3Rails4.scout_transaction_uri(request))
+
+              # IP Spoofing Protection can throw an exception, just move on w/o remote ip
+              req.context.add_user(:ip => request.remote_ip) rescue nil
+              req.set_headers(request.headers)
+
+              req.web!
+
+              resolved_name = scout_action_name(*args)
+              STDOUT.puts("Got action name: #{resolved_name}")
+              req.start_layer( ScoutApm::Layer.new("Controller", "#{controller_path}/#{resolved_name}") )
+              STDOUT.puts "Starting controller layer"
+              begin
+                super
+              rescue
+                req.error!
+                raise
+              ensure
+                req.stop_layer
+                STDOUT.puts "Stopping controller layer"
+              end
+            end
+          end
+        end
       end
 
       # Given an +ActionDispatch::Request+, formats the uri based on config settings.
@@ -57,73 +106,18 @@ module ScoutApm
     end
 
     module ActionControllerMetalInstruments
-      def process_action(*args)
-        req = ScoutApm::RequestManager.lookup
-        current_layer = req.current_layer
+      include ScoutApm::Instruments::ActionControllerRails3Rails4.build_instrument_module
 
-        # Check if this this request is to be reported instantly
-        if instant_key = request.cookies['scoutapminstant']
-          Agent.instance.logger.info "Instant trace request with key=#{instant_key} for path=#{path}"
-          req.instant_key = instant_key
-        end
-
-        if current_layer.type == "Controller"
-          # Don't start a new layer if ActionController::API or ActionController::Base handled it already.
-          STDOUT.puts "Skipping in metal"
-          super
-        else
-          req.annotate_request(:uri => ScoutApm::Instruments::ActionControllerRails3Rails4.scout_transaction_uri(request))
-
-          # IP Spoofing Protection can throw an exception, just move on w/o remote ip
-          req.context.add_user(:ip => request.remote_ip) rescue nil
-          req.set_headers(request.headers)
-
-          req.web!
-
-          action_name = args[0]
-          req.start_layer( ScoutApm::Layer.new("Controller", "#{controller_path}/#{action_name}") )
-          STDOUT.puts "Metal Instruments: Starting Layer"
-          begin
-            super
-          rescue
-            req.error!
-            raise
-          ensure
-          STDOUT.puts "Metal Instruments: Stopping Layer"
-            req.stop_layer
-          end
-        end
+      def scout_action_name(*args)
+        action_name = args[0]
       end
-
     end
 
     module ActionControllerRails3Rails4Instruments
-      def process_action(*args)
-        req = ScoutApm::RequestManager.lookup
-        current_layer = req.current_layer
+      include ScoutApm::Instruments::ActionControllerRails3Rails4.build_instrument_module
 
-        if current_layer.type == "Controller"
-          # Don't start a new layer if metal got it.
-          STDOUT.puts "Skipping in ControllerInstruments"
-          super
-        else
-          req.annotate_request(:uri => ScoutApm::Instruments::ActionControllerRails3Rails4.scout_transaction_uri(request))
-          req.context.add_user(:ip => request.remote_ip) rescue nil
-          req.set_headers(request.headers)
-          req.web!
-
-          req.start_layer( ScoutApm::Layer.new("Controller", "#{controller_path}/#{action_name}") )
-          STDOUT.puts "ControllerInstruments: Starting Layer"
-          begin
-            super
-          rescue
-            req.error!
-            raise
-          ensure
-            STDOUT.puts "ControllerInstruments: Stopping Layer"
-            req.stop_layer
-          end
-        end
+      def scout_action_name(*args)
+        action_name
       end
     end
   end
