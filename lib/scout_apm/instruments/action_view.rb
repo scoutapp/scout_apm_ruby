b2e0731c249e35e358a7889e96bb3bfd5862a7be
Chris Schneider <chris@scoutapp.com>
2017-08-15 11:56:12 -0600

    Add TracerPrepend to use Prepended Modules
    Use that in ActionView to solve an incompatibility with the `jb` gem
    which uses Module.prepend on the same methods we instrument, causing a
    problem when we used the older style alias_method approach to
    instrumentation.
    
    This is not compatible with Ruby 1.8 or 1.9. A dynamic switching or some
    other logic is needed to make this work with those older rubies.

--- lib/scout_apm/instruments/action_view.rb
+++ lib/scout_apm/instruments/action_view.rb
@@ -19,25 +19,26 @@ module ScoutApm
         if defined?(::ActionView) && defined?(::ActionView::PartialRenderer)
           ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::PartialRenderer"
           ::ActionView::PartialRenderer.class_eval do
-            include ScoutApm::Tracer
+            include ScoutApm::TracerPrepend
 
             instrument_method :render_partial,
               :type => "View",
-              :name => '#{@template.virtual_path rescue "Unknown Partial"}/Rendering',
+              :name => ->(obj, args) { "#{obj.instance_variable_get("@template").virtual_path rescue "Unknown Partial"}/Rendering" },
               :scope => true
 
             instrument_method :collection_with_template,
               :type => "View",
-              :name => '#{@template.virtual_path rescue "Unknown Collection"}/Rendering',
+              :name => ->(obj, args) { "#{obj.instance_variable_get("@template").virtual_path rescue "Unknown Collection"}/Rendering" },
               :scope => true
           end
 
           ScoutApm::Agent.instance.logger.info "Instrumenting ActionView::TemplateRenderer"
           ::ActionView::TemplateRenderer.class_eval do
-            include ScoutApm::Tracer
+            include ScoutApm::TracerPrepend
+
             instrument_method :render_template,
               :type => "View",
-              :name => '#{args[0].virtual_path rescue "Unknown"}/Rendering',
+              :name => ->(obj, args) { "#{args[0].virtual_path rescue "Unknown"}/Rendering" },
               :scope => true
           end
         end
---
+++ lib/scout_apm/tracer_prepend.rb
@@ -0,0 +1,57 @@
+# Provides helpers to wrap sections of code in instrumentation using Ruby 2.0 Prepend approach
+
+module ScoutApm
+  module TracerPrepend
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+
+    module ClassMethods
+      # Type: the Layer type - "View" or similar
+      # Name: specific name - "users/_gravatar". The object must respond to "#to_s". This allows us to be more efficient - in most cases, the metric name isn't needed unless we are processing a slow transaction.
+      # A Block: The code to be instrumented
+      #
+      # Options:
+      # * :ignore_children - will not instrument any method calls beneath this call. Example use case: InfluxDB uses Net::HTTP, which is instrumented. However, we can provide more specific data if we know we're doing an influx call, so we'd rather just instrument the Influx call and ignore Net::HTTP.
+      #   when rendering the transaction tree in the UI.
+      # * :desc - Additional capture, SQL, or HTTP url or similar
+      # * :scope - set to true if you want to make this layer a subscope
+      def instrument_method(method_name, options = {})
+        mod = create_instrumented_module(method_name, options)
+        self.prepend(mod)
+      end
+
+      private
+
+      # prepend this module into the class you'd like to instrument
+      def create_instrumented_module(method_name, options)
+        ScoutApm::Agent.instance.logger.info "Instrumenting #{method_name}"
+        type = options[:type] || "Custom"
+        name = options[:name] || "#{self.name}/#{method_name.to_s}"
+
+        Module.new do
+          define_method method_name do |*args, &block|
+            resolved_name = name.respond_to?(:call) ? name.call(self, args) : name
+
+            layer = ScoutApm::Layer.new(type, resolved_name)
+            layer.desc = options[:desc] if options[:desc]
+            layer.subscopable!          if options[:scope]
+
+            req = ScoutApm::RequestManager.lookup
+            req.start_layer(layer)
+            req.ignore_children! if options[:ignore_children]
+
+            STDOUT.puts "in instruemnetation for #{type} / #{resolved_name}"
+
+            begin
+              super(*args, &block)
+            ensure
+              req.acknowledge_children! if options[:ignore_children]
+              req.stop_layer
+            end
+          end
+        end
+      end
+    end
+  end
+end
