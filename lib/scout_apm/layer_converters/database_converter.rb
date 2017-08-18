53311171bab84007ae8b4d714b3362f2fb8ac75a
David Anderson <dave@dubkat.com>
2017-08-18 13:10:31 -0600

    Create DbQueryMetricSet directly in DatabaseConverter

--- lib/scout_apm/layer_converters/database_converter.rb
+++ lib/scout_apm/layer_converters/database_converter.rb
@@ -12,17 +12,26 @@ module ScoutApm
       end
 
       def select_database_layers
-        metric_ary = Array.new
+        db_layers = Array.new
 
         walker.walk do |layer|
           next if skip_layer?(layer) || layer.annotations.nil?
-          metric_ary << layer if layer.type == 'ActiveRecord'
+          db_layers << layer if layer.type == 'ActiveRecord'
         end
-        metric_ary
+        db_layers
       end
 
+      # Takes an array of ActiveRecord layers, creates new DbQueryMetricStats and combines
+      # them into a new DbQueryMetricSet.
+      # This might be a bit much overhead. Make a new method that can report/combine the raw numbers without
+      # the intermediate creation of a DbQueryMetricStats object
       def db_query_metric_set_from_layers(database_layers)
-        DbQueryMetricSet.new.absorb_layers!(database_layers)
+        db_query_metric_set = DbQueryMetricSet.new
+        database_layers.each do |l|
+          db_query_metric_stats = DbQueryMetricStats.new(l.name.model, l.name.normalized_operation, 1, l.total_call_time, l.annotations[:record_count])
+          db_query_metric_set.combine!(db_query_metric_stats)
+        end
+        db_query_metric_set
       end
     end
   end
