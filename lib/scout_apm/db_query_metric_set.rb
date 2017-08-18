f20daf21a92365f36d132f60fb01f307fa39bcfa
David Anderson <dave@dubkat.com>
2017-08-18 13:03:08 -0600

    Add `min_rows_returend` and `max_rows_returned` to DbQueryMetricStats

--- lib/scout_apm/db_query_metric_set.rb
+++ lib/scout_apm/db_query_metric_set.rb
@@ -14,7 +14,9 @@ module ScoutApm
       lookup(other).combine!(other)
     end
 
-    # Takes an array of ActiveRecord layers, creates new DbQueryMetricStats and combines them into this Set
+    # Takes an array of ActiveRecord layers, creates new DbQueryMetricStats and combines them into this Set.
+    # This might be a bit much overhead. Make a new method that can report/combine the raw numbers without
+    # the intermediate creation of a DbQueryMetricStats object
     def absorb_layers!(database_layers)
       database_layers.each do |l|
         db_query_metric_stats = DbQueryMetricStats.new(l.name.model, l.name.normalized_operation, 1, l.total_call_time, l.annotations[:record_count])
--- lib/scout_apm/db_query_metric_stats.rb
+++ lib/scout_apm/db_query_metric_stats.rb
@@ -12,6 +12,9 @@ module ScoutApm
     attr_accessor :min_call_time
     attr_accessor :max_call_time
 
+    attr_accessor :min_rows_returned
+    attr_accessor :max_rows_returned
+
     attr_accessor :histogram
 
     def initialize(model_name, operation, call_count, call_time, rows_returned)
@@ -25,6 +28,11 @@ module ScoutApm
       @min_call_time = call_time
       @max_call_time = call_time
 
+      @min_rows_returned = rows_returned
+      @max_rows_returned = rows_returned
+
+      # Should we have a histogram for timing, and one for rows_returned?
+      # This histogram is for call_time
       @histogram = NumericHistogram.new(DEFAULT_HISTOGRAM_SIZE)
       @histogram.add(call_time)
     end
@@ -39,8 +47,13 @@ module ScoutApm
       self.call_count += other.call_count
       self.rows_returned += self.rows_returned
       self.call_time += other.call_time
+
       self.min_call_time = other.min_call_time if self.min_call_time.zero? or other.min_call_time < self.min_call_time
       self.max_call_time = other.max_call_time if other.max_call_time > self.max_call_time
+
+      self.min_rows_returned = other.min_rows_returned if self.min_rows_returned.zero? or other.min_rows_returned < self.min_rows_returned
+      self.max_rows_returned = other.max_rows_returned if other.max_rows_returned > self.max_rows_returned
+
       self.histogram.combine!(other.histogram)
       self
     end
@@ -48,7 +61,7 @@ module ScoutApm
     # To avoid conflicts with different JSON libaries handle JSON ourselves.
     # Time-based metrics are converted to milliseconds from seconds.
     def to_json(*a)
-       %Q[{"call_count":#{call_count},"total_call_time":#{call_time*1000},"min_call_time":#{min_call_time*1000},"max_call_time":#{max_call_time*1000},"rows_returned":#{rows_returned}}, "histogram":#{histogram.as_json}]
+       %Q[{"call_count":#{call_count},"total_call_time":#{call_time*1000},"min_call_time":#{min_call_time*1000},"max_call_time":#{max_call_time*1000},"total_rows_returned":#{rows_returned},"min_rows_returned":#{min_rows_returned},"max_rows_returned":#{max_rows_returned},"histogram":#{histogram.as_json}}]
     end
 
     def as_json
