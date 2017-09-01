c248e3d4cf610cdd81e7e84266df3586a3fae2c5
Chris Schneider <chris@scoutapp.com>
2017-09-01 11:05:40 -0600

    Send an array of db_query_metrics, not a hash

--- lib/scout_apm/serializers/db_query_serializer_to_json.rb
+++ lib/scout_apm/serializers/db_query_serializer_to_json.rb
@@ -8,7 +8,7 @@ module ScoutApm
       end
 
       def as_json
-        Hash[db_query_metrics.map{|_k, v| v }.map(&:as_json)]
+        db_query_metrics.map{|_k, v| v.as_json }
       end
     end
   end
