5ee12f0fd75264532990242dcde9e8901a54e342
Chris Schneider <chris@scoutapp.com>
2017-10-10 14:29:59 -0600

    Don't try to as_json a Time object
    If you do it when using Oj gem + `Oj.mimic_JSON`, it outputs a weird
    structure instead of a String.
    
    The recursive as_json call is only useful anyway to find nested data
    structures, and Time is not one of those, so no loss here, other than
    incrementally adding some complexity.

--- lib/scout_apm/attribute_arranger.rb
+++ lib/scout_apm/attribute_arranger.rb
@@ -13,11 +13,20 @@ module ScoutApm
           attribute_hash[attribute] = subject.bucket_name
         when Symbol
           data = subject.send(attribute)
-          if data.respond_to?(:as_json)
-            attribute_hash[attribute] = data.as_json
-          else
-            attribute_hash[attribute] = data
-          end
+          attribute_hash[attribute] =
+            case data
+            # Never try to `as_json` a time object, since it'll break if the
+            # app has the Oj gem set to mimic_JSON, and there's never
+            # anything interesting nested inside of a Time obj. We just want
+            # the ISO8601 string (which happens later in the payload
+            # serializing process)
+            when Time
+              data
+            when data.respond_to?(:as_json)
+              data.as_json
+            else
+              data
+            end
         end
         attribute_hash
       end
