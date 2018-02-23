f695b4bdf5af6c565bc5b8d6c833197f24e7a4af
Andy Lindeman <andy@lindeman.io>
2018-02-23 02:19:41 +0000

    Renames test file so it runs, deletes obsolete test

--- test/unit/tracked_request_test.rb
+++ test/unit/tracked_request_test.rb
@@ -10,19 +10,6 @@ class TrackedRequestDumpAndLoadTest < Minitest::Test
     loaded = Marshal.load(dumped)
     assert_false loaded.nil?
   end
-
-  def test_restore_store
-    faux_store = ScoutApm::FakeStore.new
-    context = ScoutApm::AgentContext.new
-    tr = ScoutApm::TrackedRequest.new(context, faux_store)
-    assert_equal faux_store, tr.instance_variable_get("@store")
-
-    tr.prepare_to_dump!
-    assert_nil tr.instance_variable_get("@store")
-
-    tr.restore_store
-    assert_equal context.store, tr.instance_variable_get("@store")
-  end
 end
 
 class TrackedRequestLayerManipulationTest < Minitest::Test
