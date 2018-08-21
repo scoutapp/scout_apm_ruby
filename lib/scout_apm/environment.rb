ebd53f5f13a03be8d5851628eaa59f297dc34d4e
Chris Schneider <chris@scoutapp.com>
2018-08-21 16:35:35 -0600

    Initial Shoryuken support

--- lib/scout_apm/environment.rb
+++ lib/scout_apm/environment.rb
@@ -26,6 +26,7 @@ module ScoutApm
     BACKGROUND_JOB_INTEGRATIONS = [
       ScoutApm::BackgroundJobIntegrations::Resque.new,
       ScoutApm::BackgroundJobIntegrations::Sidekiq.new,
+      ScoutApm::BackgroundJobIntegrations::Shoryuken.new,
       ScoutApm::BackgroundJobIntegrations::DelayedJob.new,
     ]
 
