0746d7eb36e423e65ccff860887f1d969d80dfb7
Chris Schneider <chris@christopher-schneider.com>
2018-09-06 11:37:01 -0600

    Add basic Sneakers support (#216)
    When naming the worker class, this attempts to be clever around
    ActiveJob vs direct `work` calls.

--- lib/scout_apm.rb
+++ lib/scout_apm.rb
@@ -58,6 +58,7 @@ require 'scout_apm/server_integrations/null'
 require 'scout_apm/background_job_integrations/sidekiq'
 require 'scout_apm/background_job_integrations/delayed_job'
 require 'scout_apm/background_job_integrations/resque'
+require 'scout_apm/background_job_integrations/sneakers'
 
 require 'scout_apm/framework_integrations/rails_2'
 require 'scout_apm/framework_integrations/rails_3_or_4'
