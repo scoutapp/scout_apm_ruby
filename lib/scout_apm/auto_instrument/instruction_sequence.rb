b7535956580721ce388cce084dbc6491f5f616a6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-01-27 14:52:05 +1300

    Restructure code using parser gem and added tests.

---
+++ lib/scout_apm/auto_instrument/instruction_sequence.rb
@@ -0,0 +1,27 @@
+
+require 'scout_apm/auto_instrument/rails'
+
+module ScoutApm
+  module AutoInstrument
+    module InstructionSequence
+      CONTROLLER_PATH_PATTERN = /\/app\/controllers\/.*_controller.rb$/
+
+      def load_iseq(path)
+        if path =~ CONTROLLER_PATH_PATTERN
+          new_code = Rails.rewrite(path)
+          return compile(new_code, File.basename(path), path)
+        else
+          return compile_file(path)
+        end
+      end
+    end
+
+    # This should work (https://bugs.ruby-lang.org/issues/15572), but it doesn't.
+    # RubyVM::InstructionSequence.extend(InstructionSequence)
+
+    # So we do this instead:
+    class << ::RubyVM::InstructionSequence
+      prepend InstructionSequence
+    end
+  end
+end
---
+++ lib/scout_apm/auto_instrument/rails.rb
@@ -0,0 +1,70 @@
+
+# In order for this to work, you must add `gem 'parser'` to your Gemfile.
+require 'parser/current'
+
+module ScoutApm
+  module AutoInstrument
+    module Rails
+      def self.rewrite(path, code = nil)
+        code ||= File.read(path)
+
+        ast = Parser::CurrentRuby.parse(code)
+
+        # pp ast
+
+        buffer = Parser::Source::Buffer.new(path)
+        buffer.source = code
+
+        rewriter = Rewriter.new
+
+        # Rewrite the AST, returns a String with the new form.
+        rewriter.rewrite(buffer, ast)
+      end
+
+      class Rewriter < ::Parser::TreeRewriter
+        def initialize
+          super
+
+          @nesting = []
+          @method = []
+        end
+
+        def parent_type?(type, up = 1)
+          parent = @nesting[@nesting.size - up - 1] and parent.type == type
+        end
+
+        # def on_block(node)
+        #   line = node.location.line || 'line?'
+        #   column = node.location.column || 'column?'
+        #   method_name = node.children[0].children[1] || '*unknown*'
+        #
+        #   wrap(node.location.expression, "::ScoutApm::Instruments::AutoInstruments.dynamic_layer('#{method_name}:l#{line}:c#{column}'){", "}")
+        # end
+
+        def on_send(node)
+          return if parent_type?(:block) or @method.empty?
+
+          line = node.location.line || 'line?'
+          column = node.location.column || 'column?'
+          method_name = node.children[1] || '*unknown*'
+
+          wrap(node.location.expression, "::ScoutApm::Instruments::AutoInstruments.dynamic_layer('#{method_name}:l#{line}:c#{column}'){", "}")
+        end
+
+        def process(node)
+          @nesting.push(node)
+
+          if node and node.type == :def
+            @method.push(node)
+            super
+            @method.pop
+          else
+            super
+          end
+
+          @nesting.pop
+        end
+      end
+    end
+  end
+end
