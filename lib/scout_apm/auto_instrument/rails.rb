3eed875eba60d4aa2ed4be4916b68959f11dd7ef
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-01-31 12:55:49 +1300

    Whitespace.

--- lib/scout_apm/auto_instrument/rails.rb
+++ lib/scout_apm/auto_instrument/rails.rb
@@ -24,7 +24,7 @@ module ScoutApm
 
           # Keeps track of the parent - child relationship between nodes:
           @nesting = []
-          
+
           # The stack of method nodes (type :def):
           @method = []
         end
