3f7a1583d92e5217842a6057ffdc51a8b513c42f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-01-31 13:12:51 +1300

    Better handling of parser gem dependency.

--- lib/scout_apm/auto_instrument.rb
+++ lib/scout_apm/auto_instrument.rb
@@ -3,8 +3,10 @@ begin
 	# In order for this to work, you must add `gem 'parser'` to your Gemfile.
 	require 'parser/current'
 	
+	raise LoadError, "Parser::TreeRewriter was not defined" unless defined?(Parser::TreeRewriter)
+	
 	require 'scout_apm/auto_instrument/instruction_sequence'
 	require 'scout_apm/auto_instrument/layer'
 rescue LoadError
-	warn "ScoutApm::AutoInstrument requires `gem 'parser'` to be present. Skipping."
+	warn "ScoutApm::AutoInstrument requires `gem 'parser'` to be present: #{$!}. Skipping."
 end
