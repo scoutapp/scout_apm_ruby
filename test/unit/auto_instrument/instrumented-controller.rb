b10d6ec0c026b94b21e20232959ba1b3b2f53390
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-05 13:50:54 +1300

    Update test fixtures.

--- test/unit/auto_instrument/instrumented-controller.rb
+++ test/unit/auto_instrument/instrumented-controller.rb
@@ -3,47 +3,47 @@ class ClientsController < ApplicationController
   before_action :check_authorization
 
   def index
-    if ::ScoutApm::AutoInstrument('==:l6:c7'){params[:status] == "activated"}
-      @clients = ::ScoutApm::AutoInstrument('activated:l7:c17'){Client.activated}
+    if ::ScoutApm::AutoInstrument('==:L6:C7'){params[:status] == "activated"}
+      @clients = ::ScoutApm::AutoInstrument('activated:L7:C17'){Client.activated}
     else
-      @clients = ::ScoutApm::AutoInstrument('inactivated:l9:c17'){Client.inactivated}
+      @clients = ::ScoutApm::AutoInstrument('inactivated:L9:C17'){Client.inactivated}
     end
   end
 
   def create
-    @client = ::ScoutApm::AutoInstrument('new:l14:c14'){Client.new(params[:client])}
-    if ::ScoutApm::AutoInstrument('save:l15:c7'){@client.save}
-      ::ScoutApm::AutoInstrument('redirect_to:l16:c6'){redirect_to @client}
+    @client = ::ScoutApm::AutoInstrument('new:L14:C14'){Client.new(params[:client])}
+    if ::ScoutApm::AutoInstrument('save:L15:C7'){@client.save}
+      ::ScoutApm::AutoInstrument('redirect_to:L16:C6'){redirect_to @client}
     else
       # This line overrides the default rendering behavior, which
       # would have been to render the "create" view.
-      ::ScoutApm::AutoInstrument('render:l20:c6'){render "new"}
+      ::ScoutApm::AutoInstrument('render:L20:C6'){render "new"}
     end
   end
 
   def edit
-    @client = ::ScoutApm::AutoInstrument('new:l25:c14'){Client.new(params[:client])}
+    @client = ::ScoutApm::AutoInstrument('new:L25:C14'){Client.new(params[:client])}
 
-    if ::ScoutApm::AutoInstrument('post?:l27:c7'){request.post?}
-      ::ScoutApm::AutoInstrument('transaction:l28:c6'){@client.transaction do
+    if ::ScoutApm::AutoInstrument('post?:L27:C7'){request.post?}
+      ::ScoutApm::AutoInstrument('transaction:L28:C6'){@client.transaction do
         @client.update_attributes(params[:client])
       end}
     end
   end
 
   def data
-    @clients = ::ScoutApm::AutoInstrument('all:l35:c15'){Client.all}
+    @clients = ::ScoutApm::AutoInstrument('all:L35:C15'){Client.all}
 
-    formatter = ::ScoutApm::AutoInstrument('proc:l37:c16'){proc do |row|
+    formatter = ::ScoutApm::AutoInstrument('proc:L37:C16'){proc do |row|
       row.to_json
     end}
 
-    ::ScoutApm::AutoInstrument('respond_with:l41:c4'){respond_with @clients.each(&formatter).join("\n"), :content_type => 'application/json; boundary=NL'}
+    ::ScoutApm::AutoInstrument('respond_with:L41:C4'){respond_with @clients.each(&formatter).join("\n"), :content_type => 'application/json; boundary=NL'}
   end
   
   def things
     x = {}
     x[:this] ||= 'foo'
-    x[:that] &&= ::ScoutApm::AutoInstrument('size:l47:c17'){'foo'.size}
+    x[:that] &&= ::ScoutApm::AutoInstrument('size:L47:C17'){'foo'.size}
   end
 end
