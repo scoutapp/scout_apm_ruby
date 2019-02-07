1ca2baec27ec3fdb36b24b1649872918496a7fd7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-07 13:13:23 +1300

    Don't load ScoutApm::AutoInstrument on older Rubies.

--- lib/scout_apm/auto_instrument.rb
+++ lib/scout_apm/auto_instrument.rb
@@ -1,12 +1,16 @@
 
-begin
-	# In order for this to work, you must add `gem 'parser'` to your Gemfile.
-	require 'parser/current'
-	
-	raise LoadError, "Parser::TreeRewriter was not defined" unless defined?(Parser::TreeRewriter)
-	
-	require 'scout_apm/auto_instrument/instruction_sequence'
-	require 'scout_apm/auto_instrument/layer'
-rescue LoadError
-	warn "ScoutApm::AutoInstrument requires `gem 'parser'` to be present: #{$!}. Skipping."
+if RUBY_VERSION >= "2.3"
+  begin
+    # In order for this to work, you must add `gem 'parser'` to your Gemfile.
+    require 'parser/current'
+
+    raise LoadError, "Parser::TreeRewriter was not defined" unless defined?(Parser::TreeRewriter)
+
+    require 'scout_apm/auto_instrument/instruction_sequence'
+    require 'scout_apm/auto_instrument/layer'
+  rescue LoadError
+    warn "ScoutApm::AutoInstrument requires `gem 'parser'` to be present: #{$!}. Skipping."
+  end
+else
+  warn "ScoutApm::AutoInstrument requires Ruby >= v2.3. Skipping."
 end
