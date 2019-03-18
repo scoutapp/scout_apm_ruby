be99eb74acba5a1a19f52344c9cdbed97019858d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-18 21:43:38 +1300

    Ensure that files can be required in isolation. Fixes #250.

--- lib/scout_apm/auto_instrument.rb
+++ lib/scout_apm/auto_instrument.rb
@@ -1,16 +1,6 @@
 
 if RUBY_VERSION >= "2.3"
-  begin
-    # In order for this to work, you must add `gem 'parser'` to your Gemfile.
-    require 'parser/current'
-
-    raise LoadError, "Parser::TreeRewriter was not defined" unless defined?(Parser::TreeRewriter)
-
-    require 'scout_apm/auto_instrument/instruction_sequence'
-    require 'scout_apm/auto_instrument/layer'
-  rescue LoadError
-    warn "ScoutApm::AutoInstrument requires `gem 'parser'` to be present: #{$!}. Skipping."
-  end
+  require 'scout_apm/auto_instrument/instruction_sequence'
 else
   warn "ScoutApm::AutoInstrument requires Ruby >= v2.3. Skipping."
 end
