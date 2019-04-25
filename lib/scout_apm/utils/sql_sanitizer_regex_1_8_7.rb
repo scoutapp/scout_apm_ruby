279e2281c390e8837c9b182984d59644af7ad28d
Bohdan Benovskyi <bohdanben@interlink-ua.com>
2019-04-25 18:43:15 +0300

    Change regex.

--- lib/scout_apm/utils/sql_sanitizer_regex_1_8_7.rb
+++ lib/scout_apm/utils/sql_sanitizer_regex_1_8_7.rb
@@ -10,7 +10,7 @@ module ScoutApm
       PSQL_REMOVE_INTEGERS = /\b\d+\b/.freeze
       PSQL_PLACEHOLDER = /\$\d+/.freeze
       PSQL_IN_CLAUSE = /IN\s+\(\?[^\)]*\)/.freeze
-      PSQL_AFTER_WHERE = /(where ).*/i.freeze
+      PSQL_AFTER_WHERE = /(?:WHERE\s+).*?(?:SELECT|$)/i.freeze
 
       MYSQL_VAR_INTERPOLATION = %r|\[\[.*\]\]\s*$|.freeze
       MYSQL_REMOVE_INTEGERS = /\b\d+\b/.freeze
