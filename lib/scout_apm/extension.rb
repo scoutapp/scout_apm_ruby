e01494472bb291fa04128c6d9020f07940b879cd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-07-03 20:07:03 +1200

    Fixups.

--- lib/scout_apm/extension.rb
+++ lib/scout_apm/extension.rb
@@ -2,33 +2,33 @@ module ScoutApm
   # This provides an abstraction which uses Module#prepend on Ruby that supports it, and provides a small meta-programming hack for Rubies that don't that achieves something very similar.
   module Extension
     if Module.respond_to?(:prepend)
-      def apply(klass)
-        ScoutApm::Agent.instance.context.logger.info "Instrumenting #{klass.inspect}"
-    
-        klass.prepend(self)
+      def self.apply klass, &block
+        # ScoutApm::Agent.instance.context.logger.info "Instrumenting #{klass.inspect}"
+        
+        extension = Module.new
+        extension.module_eval(&block)
+        klass.prepend(extension)
       end
     else
-      def extensions_module_for(klass)
-        klass.send(:include, Module.new)
-      end
-      
-      def apply(klass)
-        ScoutApm::Agent.instance.context.logger.info "Instrumenting #{klass.inspect}"
+      def self.apply klass, &block
+        # ScoutApm::Agent.instance.context.logger.info "Instrumenting #{klass.inspect}"
 
-        self.instance_methods.each do |name|
+        wrapper = Module.new
+        wrapper.module_eval(&block)
+
+        extension = Module.new
+        klass.send(:include, extension)
+
+        wrapper.instance_methods.each do |name|
           original_method = klass.instance_method(name)
-          wrapper_method = self.instance_method(name)
-          
-          parent = extensions_module_for(klass)
-          
-          parent.send(:define_method, name) do |*args, &block|
+          klass.send(:undef_method, name)
+
+          extension.send(:define_method, name) do |*args, &block|
             original_method.bind(self).call(*args, &block)
           end
-          
-          klass.send(:define_method, name, wrapper_method)
-          
-          return klass
         end
+
+        klass.class_eval(&block)
       end
     end
   end
--- test/unit/extension_test.rb
+++ test/unit/extension_test.rb
@@ -2,35 +2,31 @@ require 'test_helper'
 require 'scout_apm/extension'
 
 class ExtensionTest < Minitest::Test
-	class Base
-		def initialize
-			@sequence = []
-		end
-		
-		attr :sequence
-		
-		def log(arg)
-			@sequence << arg
-		end
-	end
-	
-	module Overrides
-		extend ScoutApm::Extension
-		
-		def log(arg)
-			@sequence << :before
-			super
-			@sequence << :after
-		end
-	end
-	
-	Overrides.apply(Base)
-	
-	def test_module_apply
-		base = Base.new
-		
-		base.log(:super)
-		
-		assert_equal base.sequence, [:before, :super, :after]
-	end
+  class Base
+    def initialize
+      @sequence = []
+    end
+
+    attr :sequence
+
+    def log(arg)
+      @sequence << arg
+    end
+  end
+
+  ScoutApm::Extension.apply(Base) do
+    def log(arg)
+      @sequence << :before
+      super
+      @sequence << :after
+    end
+  end
+
+  def test_module_apply
+    base = Base.new
+
+    base.log(:super)
+
+    assert_equal [:before, :super, :after], base.sequence
+  end
 end
