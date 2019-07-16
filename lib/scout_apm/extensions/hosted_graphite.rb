ff9518947dc322cf0e228998de3f0dd56ba328c0
naohiro-t <takahashi.naohiro1222@gmail.com>
2019-07-16 18:49:34 +0100

    add tags to the message

--- lib/scout_apm/extensions/hosted_graphite.rb
+++ lib/scout_apm/extensions/hosted_graphite.rb
@@ -2,41 +2,48 @@ module ScoutApm
   module Extensions
     class HostedGraphite
       def self.install
-        ScoutApm::Extensions::Config.add_periodic_callback(self)
+        ScoutApm::Extensions::Config.add_periodic_callback(self.new)
       end
 
-      def self.call(agent_context, reporting_period, metadata)
+      def initialize
+        @socket = UDPSocket.new
+      end
+
+      def call(agent_context, reporting_period, metadata)
         @agent_context = agent_context
 
         reporting_period.metrics_payload.each do |meta, stat|
-          case metric_type(meta.metric_name)
+          @metric_name = meta.metric_name
+
+          case metric_type
           when 'web'
-            send('web.total_call_time', stat.total_call_time)
-            send('web.throughput', stat.call_count)
+            send("web.total_call_time", stat.total_call_time)
+            send("web.throughput", stat.call_count)
           end
         end
       end
 
       private
-      def self.send(metric_path, value)
-        @@socket ||= UDPSocket.new
-        @@socket.send "#{prefix}#{metric_path} #{value}\n", 0, "carbon.hostedgraphite.com", 2003
+      def send(metric_path, value)
+        message = "#{prefix}#{metric_path}#{tags} #{value}"
+        logger.debug "Sending #{message} to carbon.hostedgraphite.com"
+        @socket.send "#{message}\n", 0, "carbon.hostedgraphite.com", 2003
       end
 
-      def self.prefix
-        "#{ENV['HOSTED_GRAPHITE_API_KEY']}.scout."
+      def prefix
+        "#{ENV['HOSTED_GRAPHITE_API_KEY']}.scout.#{app_name}."
       end
 
-      def self.hostname
+      def hostname
         @agent_context.environment.hostname.gsub(/\./, '_')
       end
 
-      def self.app_name
+      def app_name
         @agent_context.config.value('name').gsub(/\./, '_')
       end
 
-      def self.metric_type(metric_name)
-        case metric_name
+      def metric_type
+        case @metric_name
         when 'Memory/Physical'
           'memory'
         when 'CPU/Utilization'
@@ -44,6 +51,23 @@ module ScoutApm
         when /^Controller\//
           'web'
         end
+      end
+
+      def tags
+        ";hostname=#{hostname}" +
+        case metric_type
+        when 'web'
+          ";endpoint=#{endpoint}"
+        end
+      end
+
+      # tag can't include slash
+      def endpoint
+        @metric_name.gsub(/\//, '_')
+      end
+
+      def logger
+        @agent_context.logger
       end
     end
   end
