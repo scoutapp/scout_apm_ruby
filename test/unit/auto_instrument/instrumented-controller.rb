e0b7b01a9d5a18a23d0d8d9861d3fbea7af6d5d0
Derek Haynes <derek.haynes@gmail.com>
2019-08-25 13:47:34 -0600

    Fixing broken tests by normalizing backtraces

--- test/unit/auto_instrument/instrumented-controller.rb
+++ test/unit/auto_instrument/instrumented-controller.rb
@@ -3,47 +3,47 @@ class ClientsController < ApplicationController
   before_action :check_authorization
 
   def index
-    if ::ScoutApm::AutoInstrument("ClientsController\#index:6", "params[:status] == \"activated\""){params[:status] == "activated"}
-      @clients = ::ScoutApm::AutoInstrument("ClientsController\#index:7", "Client.activated"){Client.activated}
+    if ::ScoutApm::AutoInstrument("params[:status] == \"activated\"",["BACKTRACE"]){params[:status] == "activated"}
+      @clients = ::ScoutApm::AutoInstrument("Client.activated",["BACKTRACE"]){Client.activated}
     else
-      @clients = ::ScoutApm::AutoInstrument("ClientsController\#index:9", "Client.inactivated"){Client.inactivated}
+      @clients = ::ScoutApm::AutoInstrument("Client.inactivated",["BACKTRACE"]){Client.inactivated}
     end
   end
 
   def create
-    @client = ::ScoutApm::AutoInstrument("ClientsController\#create:14", "Client.new(params[:client])"){Client.new(params[:client])}
-    if ::ScoutApm::AutoInstrument("ClientsController\#create:15", "@client.save"){@client.save}
-      ::ScoutApm::AutoInstrument("ClientsController\#create:16", "redirect_to @client"){redirect_to @client}
+    @client = ::ScoutApm::AutoInstrument("Client.new(params[:client])",["BACKTRACE"]){Client.new(params[:client])}
+    if ::ScoutApm::AutoInstrument("@client.save",["BACKTRACE"]){@client.save}
+      ::ScoutApm::AutoInstrument("redirect_to @client",["BACKTRACE"]){redirect_to @client}
     else
       # This line overrides the default rendering behavior, which
       # would have been to render the "create" view.
-      ::ScoutApm::AutoInstrument("ClientsController\#create:20", "render \"new\""){render "new"}
+      ::ScoutApm::AutoInstrument("render \"new\"",["BACKTRACE"]){render "new"}
     end
   end
 
   def edit
-    @client = ::ScoutApm::AutoInstrument("ClientsController\#edit:25", "Client.new(params[:client])"){Client.new(params[:client])}
+    @client = ::ScoutApm::AutoInstrument("Client.new(params[:client])",["BACKTRACE"]){Client.new(params[:client])}
 
-    if ::ScoutApm::AutoInstrument("ClientsController\#edit:27", "request.post?"){request.post?}
-      ::ScoutApm::AutoInstrument("ClientsController\#edit:28", "@client.transaction do..."){@client.transaction do
+    if ::ScoutApm::AutoInstrument("request.post?",["BACKTRACE"]){request.post?}
+      ::ScoutApm::AutoInstrument("@client.transaction do...",["BACKTRACE"]){@client.transaction do
         @client.update_attributes(params[:client])
       end}
     end
   end
 
   def data
-    @clients = ::ScoutApm::AutoInstrument("ClientsController\#data:35", "Client.all"){Client.all}
+    @clients = ::ScoutApm::AutoInstrument("Client.all",["BACKTRACE"]){Client.all}
 
-    formatter = ::ScoutApm::AutoInstrument("ClientsController\#data:37", "proc do |row|..."){proc do |row|
+    formatter = ::ScoutApm::AutoInstrument("proc do |row|...",["BACKTRACE"]){proc do |row|
       row.to_json
     end}
 
-    ::ScoutApm::AutoInstrument("ClientsController\#data:41", "respond_with @clients.each(&formatter).join(\"\\n\"), :content_type => 'application/json; boundary=NL'"){respond_with @clients.each(&formatter).join("\n"), :content_type => 'application/json; boundary=NL'}
+    ::ScoutApm::AutoInstrument("respond_with @clients.each(&formatter).join(\"\\n\"), :content_type => 'application/json; boundary=NL'",["BACKTRACE"]){respond_with @clients.each(&formatter).join("\n"), :content_type => 'application/json; boundary=NL'}
   end
   
   def things
     x = {}
     x[:this] ||= 'foo'
-    x[:that] &&= ::ScoutApm::AutoInstrument("ClientsController\#things:47", "'foo'.size"){'foo'.size}
+    x[:that] &&= ::ScoutApm::AutoInstrument("'foo'.size",["BACKTRACE"]){'foo'.size}
   end
 end
--- test/unit/auto_instrument_test.rb
+++ test/unit/auto_instrument_test.rb
@@ -14,23 +14,32 @@ class AutoInstrumentTest < Minitest::Test
   def instrumented_source(name)
     File.read(instrumented_path(name))
   end
-  
+
+  # Autoinstruments adds a backtrace to each created layer. This is the full path to the
+  # test controller.rb file, which will be different on different environments.
+  # This normalizes backtraces across environments.
+  def normalize_backtrace(string)
+    string.gsub(/\[".+auto_instrument\/controller.rb:.+"\]/,'["BACKTRACE"]')
+  end
+
   # Use this to automatically update the test fixtures.
   def update_instrumented_source(name)
     File.write(
       instrumented_path(name),
-      ::ScoutApm::AutoInstrument::Rails.rewrite(source_path(name))
+      normalize_backtrace(::ScoutApm::AutoInstrument::Rails.rewrite(source_path(name)))
     )
   end
-  
+
   def test_rails_controller_rewrite
-    assert_equal instrumented_source("controller"), ::ScoutApm::AutoInstrument::Rails.rewrite(source_path("controller"))
-    
+    assert_equal instrumented_source("controller"),
+    normalize_backtrace(::ScoutApm::AutoInstrument::Rails.rewrite(source_path("controller")))
+
     # update_instrumented_source("controller")
   end
 
   def test_rescue_from_controller_rewrite
-    assert_equal instrumented_source("rescue_from"), ::ScoutApm::AutoInstrument::Rails.rewrite(source_path("rescue_from"))
+    assert_equal instrumented_source("rescue_from"),
+    normalize_backtrace(::ScoutApm::AutoInstrument::Rails.rewrite(source_path("rescue_from")))
 
     # update_instrumented_source("rescue_from")
   end
