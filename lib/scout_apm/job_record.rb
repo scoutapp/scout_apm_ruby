05193f00db7871d0aab6bb71124ee87a719448dc
Chris Schneider <chris@scoutapp.com>
2019-12-05 10:06:20 -0700

    Add logging when mismatched JobRecord.merge! calls happen

--- lib/scout_apm/job_record.rb
+++ lib/scout_apm/job_record.rb
@@ -33,7 +33,11 @@ module ScoutApm
     # Modifies self and returns self, after merging in `other`.
     def combine!(other)
       same_job = queue_name == other.queue_name && job_name == other.job_name
-      raise "Mismatched Merge of Background Job" unless same_job
+
+      if !same_job
+        ScoutApm::Agent.instance.logger.debug("Mismatched Merge of Background Job: (Queue #{queue_name} == #{other.queue_name}) (Name #{job_name} == #{other.job_name}) (Hash #{hash} == #{other.hash})")
+        return self
+      end
 
       @errors += other.errors
       @metric_set = metric_set.combine!(other.metric_set)
