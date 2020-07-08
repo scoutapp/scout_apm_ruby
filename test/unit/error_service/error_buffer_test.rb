38ab5f05b9a1fe61ffc1fd387b2949160751dd5f
Chris Schneider <chris@christopher-schneider.com>
2020-07-08 16:24:26 -0600

    Add error buffer to agent_context test

---
+++ test/unit/error_service/error_buffer_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class ErrorBufferTest < Minitest::Test
+  class FakeError < StandardError
+  end
+
+  def test_captures_and_stores_exceptions_and_env
+    eb = ScoutApm::ErrorService::ErrorBuffer.new(context)
+
+    eb.capture(ex, env)
+    assert eb.count == 1
+    assert eb.first.exception == ex
+    assert eb.first.env == env
+  end
+
+  #### Helpers
+
+  def context
+    ScoutApm::AgentContext.new
+  end
+
+  def env
+    {}
+  end
+
+  def ex(msg="Whoops")
+    FakeError.new(msg)
+  end
+end
