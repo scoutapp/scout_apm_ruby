80f60296868364b9fb2ba40c204da14b99b33d0b
Chris Schneider <chris@christopher-schneider.com>
2020-07-23 10:51:21 -0600

    Binding.pry has no place here

--- lib/scout_apm/middleware.rb
+++ lib/scout_apm/middleware.rb
@@ -26,7 +26,6 @@ module ScoutApm
       ScoutApm::Agent.instance.start
       @started = ScoutApm::Agent.instance.context.started? && ScoutApm::Agent.instance.background_worker_running?
     rescue => e
-      binding.pry
       ScoutApm::Agent.instance.context.logger.info("Failed to start via Middleware: #{e.message}\n\t#{e.backtrace.join("\n\t")}")
     end
   end
