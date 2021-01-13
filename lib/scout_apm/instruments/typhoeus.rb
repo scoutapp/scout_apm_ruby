2abfcb121836f2fa2202ba5765cf474f6624878b
Mathieu Jobin <majobin@mdsol.com>
2021-01-13 15:45:44 +0900

    experimental typhoeus support

---
+++ lib/scout_apm/instruments/typhoeus.rb
@@ -0,0 +1,57 @@
+module ScoutApm
+  module Instruments
+    class Typhoeus
+      attr_reader :context
+
+      def initialize(context)
+        @context = context
+        @installed = false
+      end
+
+      def logger
+        context.logger
+      end
+
+      def installed?
+        @installed
+      end
+
+      def install
+        if defined?(::Typhoeus) && defined?(::Typhoeus::Request)
+          @installed = true
+
+          logger.info "Instrumenting Typhoeus::Request"
+
+          ::Typhoeus::Request.class_eval do
+            include ScoutApm::Tracer
+
+            def request_with_scout_instruments(*args, &block)
+              self.class.instrument("HTTP", "request", :ignore_children => true, :desc => request_scout_description(args.first)) do
+                request_without_scout_instruments(*args, &block)
+              end
+            end
+
+            def request_scout_description(req)
+#               path = req.path
+#               path = path.path if path.respond_to?(:path)
+#
+#               # Protect against a nil address value
+#               if @address.nil?
+#                 return "No Address Found"
+#               end
+
+              max_length = ScoutApm::Agent.instance.context.config.value('instrument_http_url_length')
+#               (@address + path.split('?').first)[0..(max_length - 1)]
+              req.uri.to_s.split('?').first)[0..(max_length - 1)]
+            rescue
+              ""
+            end
+
+            alias request_without_scout_instruments request
+            alias request request_with_scout_instruments
+          end
+        end
+      end
+    end
+  end
+end
