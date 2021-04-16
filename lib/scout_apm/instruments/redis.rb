06f4de3100cfe99ec76161f36ba77002e134c9d6
Dave Anderson <dave@dubkat.com>
2021-04-16 15:29:47 -0600

    Allow prepend redis instruments

--- lib/scout_apm/instruments/redis.rb
+++ lib/scout_apm/instruments/redis.rb
@@ -12,6 +12,10 @@ module ScoutApm
         context.logger
       end
 
+      def prependable?
+        context.environment.supports_module_prepend?
+      end
+
       def installed?
         @installed
       end
@@ -20,21 +24,52 @@ module ScoutApm
         if defined?(::Redis) && defined?(::Redis::Client)
           @installed = true
 
-          logger.info "Instrumenting Redis"
+          if prependable?
+            install_using_prepend
+          else
+            install_using_tracer
+          end
+        end
+      end
 
-          ::Redis::Client.class_eval do
-            include ScoutApm::Tracer
+      def install_using_tracer
+        logger.info "Instrumenting Redis"
 
-            def call_with_scout_instruments(*args, &block)
-              command = args.first.first rescue "Unknown"
+        ::Redis::Client.class_eval do
+          include ScoutApm::Tracer
 
-              self.class.instrument("Redis", command) do
-                call_without_scout_instruments(*args, &block)
-              end
+          def call_with_scout_instruments(*args, &block)
+            command = args.first.first rescue "Unknown"
+
+            self.class.instrument("Redis", command) do
+              call_without_scout_instruments(*args, &block)
             end
+          end
 
-            alias_method :call_without_scout_instruments, :call
-            alias_method :call, :call_with_scout_instruments
+          alias_method :call_without_scout_instruments, :call
+          alias_method :call, :call_with_scout_instruments
+        end
+      end
+
+      def install_using_prepend
+        logger.info "Instrumenting Redis (prepend)"
+        ::Redis::Client.prepend(RedisClientInstruments)
+      end
+
+      module RedisClientInstruments
+        def call(*args, &block)
+          req = ScoutApm::RequestManager.lookup
+
+          command = args.first.first rescue "Unknown"
+
+          layer = ScoutApm::Layer.new("Redis", command)
+          layer.subscopable!
+
+          begin
+            req.start_layer(layer)
+            super(*args, &block)
+          ensure
+            req.stop_layer
           end
         end
       end
