60835dd1600183d46fba794bf0bf75aada819329
Chris Schneider <chris@christopher-schneider.com>
2021-05-10 22:41:12 +0000

    Remove indirection to regexes now that we don't support 1.8.7. This was because old ruby couldn't even *parse* the file

--- lib/scout_apm/utils/sql_sanitizer.rb
+++ lib/scout_apm/utils/sql_sanitizer.rb
@@ -5,12 +5,31 @@ require 'scout_apm/environment'
 module ScoutApm
   module Utils
     class SqlSanitizer
-      if ScoutApm::Environment.instance.ruby_187?
-        require 'scout_apm/utils/sql_sanitizer_regex_1_8_7'
-      else
-        require 'scout_apm/utils/sql_sanitizer_regex'
-      end
-      include ScoutApm::Utils::SqlRegex
+      MULTIPLE_SPACES    = %r|\s+|.freeze
+      MULTIPLE_QUESTIONS = /\?(,\?)+/.freeze
+
+      PSQL_VAR_INTERPOLATION = %r|\[\[.*\]\]\s*\z|.freeze
+      PSQL_REMOVE_STRINGS = /'(?:[^']|'')*'/.freeze
+      PSQL_REMOVE_INTEGERS = /(?<!LIMIT )\b\d+\b/.freeze
+      PSQL_PLACEHOLDER = /\$\d+/.freeze
+      PSQL_IN_CLAUSE = /IN\s+\(\?[^\)]*\)/.freeze
+      PSQL_AFTER_WHERE = /(?:WHERE\s+).*?(?:SELECT|\z)/im.freeze
+      PSQL_AFTER_SET = /(?:SET\s+).*?(?:WHERE|\z)/im.freeze
+
+      MYSQL_VAR_INTERPOLATION = %r|\[\[.*\]\]\s*$|.freeze
+      MYSQL_REMOVE_INTEGERS = /(?<!LIMIT )\b\d+\b/.freeze
+      MYSQL_REMOVE_SINGLE_QUOTE_STRINGS = %r{'(?:\\'|[^']|'')*'}.freeze
+      MYSQL_REMOVE_DOUBLE_QUOTE_STRINGS = %r{"(?:\\"|[^"]|"")*"}.freeze
+      MYSQL_IN_CLAUSE = /IN\s+\(\?[^\)]*\)/.freeze
+
+      SQLITE_VAR_INTERPOLATION = %r|\[\[.*\]\]\s*$|.freeze
+      SQLITE_REMOVE_STRINGS = /'(?:[^']|'')*'/.freeze
+      SQLITE_REMOVE_INTEGERS = /(?<!LIMIT )\b\d+\b/.freeze
+
+      # => "EXEC sp_executesql N'SELECT  [users].* FROM [users] WHERE (age > 50)  ORDER BY [users].[id] ASC OFFSET 0 ROWS FETCH NEXT @0 ROWS ONLY', N'@0 int', @0 = 10"
+      SQLSERVER_EXECUTESQL = /EXEC sp_executesql N'(.*?)'.*/
+      SQLSERVER_REMOVE_INTEGERS = /(?<!LIMIT )\b(?<!@)\d+\b/.freeze
+      SQLSERVER_IN_CLAUSE = /IN\s+\(\?[^\)]*\)/.freeze
 
       attr_accessor :database_engine
 
