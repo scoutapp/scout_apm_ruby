04adbc3a6279345d68fec3188aa9958f03c1bccc
Dave Anderson <dave@dubkat.com>
2022-08-11 15:02:48 -0600

    Dont prepend for Influxdb instrument

--- lib/scout_apm/instruments/influxdb.rb
+++ lib/scout_apm/instruments/influxdb.rb
@@ -20,47 +20,28 @@ module ScoutApm
         if defined?(::InfluxDB)
           @installed = true
 
-          logger.debug "Instrumenting InfluxDB"
+          logger.debug "Instrumenting InfluxDB. Prepend: #{prepend}"
 
-          if prepend
-            ::InfluxDB::Client.send(:include, ScoutApm::Tracer)
-            ::InfluxDB::HTTP.send(:prepend, InfluxHttpInstrumentationPrepend)
-          else
-            ::InfluxDB::Client.class_eval do
-              include ScoutApm::Tracer
-            end
-
-            ::InfluxDB::HTTP.module_eval do
-              def do_request_with_scout_instruments(http, req, data = nil)
-                params = req.path[1..-1].split("?").last
-                cleaned_params = CGI.unescape(params).gsub(/(\s{2,})/,' ')
-
-                self.class.instrument("InfluxDB",
-                                      "#{req.path[1..-1].split("?").first.capitalize}",
-                                      :desc => cleaned_params,
-                                      :ignore_children => true) do
-                  do_request_without_scout_instruments(http, req, data)
-                end
-              end
-
-              alias_method :do_request_without_scout_instruments, :do_request
-              alias_method :do_request, :do_request_with_scout_instruments
-            end
+          ::InfluxDB::Client.class_eval do
+            include ScoutApm::Tracer
           end
-        end
-      end
-    end
 
-    module InfluxHttpInstrumentationPrepend
-      def do_request(http, req, data = nil)
-        params = req.path[1..-1].split("?").last
-        cleaned_params = CGI.unescape(params).gsub(/(\s{2,})/,' ')
+          ::InfluxDB::HTTP.module_eval do
+            def do_request_with_scout_instruments(http, req, data = nil)
+              params = req.path[1..-1].split("?").last
+              cleaned_params = CGI.unescape(params).gsub(/(\s{2,})/,' ')
 
-        self.class.instrument("InfluxDB",
-                              "#{req.path[1..-1].split("?").first.capitalize}",
-                              :desc => cleaned_params,
-                              :ignore_children => true) do
-          super(http, req, data)
+              self.class.instrument("InfluxDB",
+                                    "#{req.path[1..-1].split("?").first.capitalize}",
+                                    :desc => cleaned_params,
+                                    :ignore_children => true) do
+                do_request_without_scout_instruments(http, req, data)
+              end
+            end
+
+            alias_method :do_request_without_scout_instruments, :do_request
+            alias_method :do_request, :do_request_with_scout_instruments
+          end
         end
       end
     end
