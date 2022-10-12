ed980cbfd6cec70f8af1e89daacb2ee87059cc0c
Dave Anderson <dave@dubkat.com>
2022-10-12 14:57:45 -0600

    Add instrumentation for Redis client v5.0+ (#459)
    * Add instrumentation for Redis client v5.0+

--- test/unit/instruments/redis_test.rb
+++ test/unit/instruments/redis_test.rb
@@ -1,23 +1,23 @@
 if (ENV["SCOUT_TEST_FEATURES"] || "").include?("instruments")
   require 'test_helper'
 
-  require 'scout_apm/instruments/redis'
+  require 'scout_apm/instruments/redis5'
 
   require 'redis'
 
   class RedisTest < Minitest::Test
     def setup
       @context = ScoutApm::AgentContext.new
-      @instance = ScoutApm::Instruments::Redis.new(@context)
+      @instance = ScoutApm::Instruments::Redis5.new(@context)
       @instrument_manager = ScoutApm::InstrumentManager.new(@context)
       @instance.install(prepend: @instrument_manager.prepend_for_instrument?(@instance.class))
     end
 
     def test_installs_using_proper_method
       if @instrument_manager.prepend_for_instrument?(@instance.class) == true
-        assert ::Redis::Client.ancestors.include?(ScoutApm::Instruments::RedisClientInstrumentationPrepend)
+        assert ::Redis::Client.ancestors.include?(ScoutApm::Instruments::Redis5ClientInstrumentationPrepend)
       else
-        assert_equal false, ::Redis::Client.ancestors.include?(ScoutApm::Instruments::RedisClientInstrumentationPrepend)
+        assert_equal false, ::Redis::Client.ancestors.include?(ScoutApm::Instruments::Redis5ClientInstrumentationPrepend)
       end
     end
   end
