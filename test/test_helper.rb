0d8db32f3d040ffe355579e6aad04c443b336d94
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2022-12-14 22:11:06 -0300

    move helpers to create rails/sinatra environment to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -65,6 +65,32 @@ class FakeEnvironment
   end
 end
 
+def fake_rails(version)
+  Kernel.const_set("Rails", Module.new)
+  Kernel.const_set("ActionController", Module.new)
+  r = Kernel.const_get("Rails")
+  r.const_set("VERSION", Module.new)
+  v = r.const_get("VERSION")
+  v.const_set("MAJOR", version)
+
+  assert_equal version, Rails::VERSION::MAJOR
+end
+
+def clean_fake_rails
+  Kernel.send(:remove_const, "Rails") if defined?(Kernel::Rails)
+  Kernel.send(:remove_const, "ActionController") if defined?(Kernel::ActionController)
+end
+
+def fake_sinatra
+  Kernel.const_set("Sinatra", Module.new)
+  s = Kernel.const_get("Sinatra")
+  s.const_set("Base", Module.new)
+end
+
+def clean_fake_sinatra
+  Kernel.const_unset("Sinatra") if defined?(Kernel::Sinatra)
+end
+
 # Helpers available to all tests
 class Minitest::Test
   def setup
