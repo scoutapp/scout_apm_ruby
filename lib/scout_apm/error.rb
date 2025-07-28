a83f89ee962f5cc397958cd6d29dc3654893dc53
Jack Rothrock <jack@scoutapm.com>
2025-07-28 14:19:53 -0600

    Guard against name collisions

--- lib/scout_apm/error.rb
+++ lib/scout_apm/error.rb
@@ -5,8 +5,11 @@ module ScoutApm
   module Error
     # Capture an exception, optionally with an environment hash. This may be a
     # Rack environment, but is not required.
+    class ScoutDefined < Exception; end
+    class Custom < ScoutDefined; end
+      
     class << self
-      def capture(exception, env={}, name: "CustomError")
+      def capture(exception, env={}, name: "ScoutApm::Error::Custom")
         context = ScoutApm::Agent.instance.context
 
         # Skip if error monitoring isn't enabled at all
@@ -38,24 +41,38 @@ module ScoutApm
       
       def define_error_class(name_str)
         # e.g., "some_error" → "SomeError", "some error" → "SomeError"
-        class_name = name_str.gsub(/(?:^|[_\s])([a-z])/) { $1.upcase }
-        Object.const_set(class_name, Class.new(Exception))
+        class_name = name_str.gsub(/(?:^|[_\s])([a-zA-Z])/) { $1.upcase }
+
+        if Object.const_defined?(class_name)
+          klass = Object.const_get(class_name)
+          return klass if klass.ancestors.include?(ScoutApm::Error::ScoutDefined)
+          
+          log_warning("Error class name '#{class_name}' is already defined. Falling back to ScoutApm::Error::Custom.")
+          return Custom
+        else
+          Object.const_set(class_name, Class.new(ScoutDefined))
+        end
+      end
+
+      def log_warning(message)
+        ScoutApm::Agent.instance.context.logger.warn(message)
       end
 
       def validate_or_create_exception(exception, name)
-        if exception.is_a?(Exception) && exception.backtrace
-          exception
-        elsif exception.is_a?(Exception)
-          exception.tap do |e|
-            e.set_backtrace(get_caller_location) # returns Array
+        return exception if exception.is_a?(Exception) && exception.backtrace
+
+        if exception.is_a?(Exception)
+          exception.tap do |e| 
+            e.set_backtrace(get_caller_location)
           end
+
         elsif exception.is_a?(String)
-          # A name of nil will cause all custom errors to be grouped together under CustomError in the UI.
           define_error_class(name).new(exception).tap do |e|
             e.set_backtrace(get_caller_location)
           end
+
         else
-          ScoutApm::Agent.instance.context.logger.warn "Invalid exception type: #{exception.class}. Expected Exception or String."
+          log_warning("Invalid exception type: #{exception.class}. Expected Exception or String.")
           nil
         end
       end
