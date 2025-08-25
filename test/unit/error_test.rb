78f64a4623ab80e89a38d35568406574c584b3b6
Jack Rothrock <jrothrock14@gmail.com>
2025-08-25 10:54:52 -0600

    Update error API to use context (#560)

--- test/unit/error_test.rb
+++ test/unit/error_test.rb
@@ -16,12 +16,19 @@ class ErrorTest < Minitest::Test
       end
       assert_equal true, ScoutApm::Error.capture("Oh no an error") # Will be of type ScoutApm::Error::Custom
       assert_equal true, ScoutApm::Error.capture("Oh no another error") # Will be of type ScoutApm::Error::Custom
+
       assert_equal true, ScoutApm::Error.capture("Something went boom", {"boom" => "yes"}, name: "boom_error")
-      assert_equal true, ScoutApm::Error.capture("No env", name: "another error")
-      assert_equal true, ScoutApm::Error.capture(ex, env)
-      assert_equal true, ScoutApm::Error.capture(exwbt, env)
-      assert_equal false, ScoutApm::Error.capture(Class, env)
-      assert_equal true, ScoutApm::Error.capture("Name collision, but", env, name: "ScoutApm")
+      ScoutApm::Context.current.instance_variable_set(:@extra, {}) # Need to reset as the context/request won't change.
+
+      assert_equal true, ScoutApm::Error.capture("No context or env", name: "another error")
+      
+      assert_equal true, ScoutApm::Error.capture(ex, context)
+      ScoutApm::Context.current.instance_variable_set(:@extra, {})
+
+      assert_equal true, ScoutApm::Error.capture(exwbt, env: env)
+
+      assert_equal false, ScoutApm::Error.capture(Class, env: env)
+      assert_equal true, ScoutApm::Error.capture("Name collision", context, env: env, name: "ScoutApm")
       
       begin
         raise StandardError, "Whoops"
@@ -44,24 +51,41 @@ class ErrorTest < Minitest::Test
 
       assert_equal "Something went boom", exceptions[2].message
       assert_equal "BoomError", exceptions[2].exception_class
+      assert_equal "yes", exceptions[2].context["boom"]
 
-      assert_equal "No env", exceptions[3].message
+      assert_equal "No context or env", exceptions[3].message
       assert_equal "AnotherError", exceptions[3].exception_class
+      assert_equal assert_empty_context, exceptions[3].context
 
       assert_equal "Whoops", exceptions[4].message
       assert_equal "ErrorTest::FakeError", exceptions[4].exception_class
+      assert_equal 123, exceptions[4].context["user_id"]
 
       assert_equal "/path/to/file.rb:10:in `method_name'", exceptions[5].trace.first
 
       assert_equal "ScoutApm::Error::Custom", exceptions[6].exception_class
+      assert_equal 123, exceptions[6].context["user_id"]
+      assert_equal "TestAgent", exceptions[6].environment["HTTP_USER_AGENT"]
 
       assert_equal "StandardError", exceptions[7].exception_class
     end
   end
 
   #### Helpers
+  def context
+    {
+      "user_id" => 123,
+    }
+  end
+
   def env
-    {}
+    {
+      "HTTP_USER_AGENT" => "TestAgent",
+    }
+  end
+
+  def assert_empty_context
+    {user: {}}
   end
 
   def ex(msg="Whoops")
