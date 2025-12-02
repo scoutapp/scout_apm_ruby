8a79706db04ae9c4f77214026e5759fa17b1df68
Lance T. Erickson <lancetarn@gmail.com>
2025-12-02 14:46:26 -0600

    Remove comment

--- lib/scout_apm/ignored_uris.rb
+++ lib/scout_apm/ignored_uris.rb
@@ -1,6 +1,4 @@
 # On first run, caches the regexes made by the `ignored` configuration setting
-# Note: For users to use regexes requiring escaped characters (e.g. \d), they
-# will need to double escape them in the YAML config file or env (e.g. \\d)
 module ScoutApm
   class IgnoredUris
     attr_reader :regex
